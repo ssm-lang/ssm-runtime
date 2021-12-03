d925ee6e7687f09f9a8fe99163b2cdf5934d5911
j-hui <j-hui@cs.columbia.edu>
2021-12-03 15:22:20 -0500

    Update a couple of examples

--- examples/clock.c
+++ examples/clock.c
@@ -1,4 +1,5 @@
-#include <ssm-types.h>
+#include <ssm-internal.h> // Needed to implement tick loop
+#include <ssm-typedefs.h> // Needed for type definitions
 #include <ssm.h>
 #include <stdio.h>
 
@@ -26,14 +27,14 @@ main =
 
 typedef struct {
   ssm_act_t act;
-  ssm_event_t *second_event;
+  ssm_event_t second_event;
   ssm_event_t timer;
   struct ssm_trigger trigger1;
 } act_second_clock_t;
 
 typedef struct {
   ssm_act_t act;
-  ssm_event_t *second_event;
+  ssm_event_t second_event;
   ssm_i32_t seconds;
   struct ssm_trigger trigger1;
 } act_report_seconds_t;
@@ -47,11 +48,12 @@ ssm_stepf_t step_second_clock;
 
 ssm_act_t *ssm_enter_second_clock(struct ssm_act *parent,
                                   ssm_priority_t priority, ssm_depth_t depth,
-                                  ssm_event_t *second_event) {
-  act_second_clock_t *cont = malloc(sizeof(*cont));
-  ssm_enter(&cont->act, step_second_clock, parent, priority, depth);
+                                  ssm_value_t second_event) {
+  act_second_clock_t *cont =
+      container_of(ssm_enter(sizeof(act_second_clock_t), step_second_clock,
+                             parent, priority, depth),
+                   act_second_clock_t, act);
   cont->second_event = second_event;
-  ssm_initialize(&cont->timer);
   return &cont->act;
 }
 
@@ -59,33 +61,31 @@ void step_second_clock(struct ssm_act *act) {
   act_second_clock_t *cont = container_of(act, act_second_clock_t, act);
   switch (act->pc) {
   case 0:
+    cont->timer = ssm_from_sv(ssm_new_sv(EVENT_VALUE));
     for (;;) {
-      ssm_assign(cont->second_event, act->priority, EVENT_VALUE);
-      ssm_later(&cont->timer, ssm_now() + SSM_SECOND, EVENT_VALUE);
+      ssm_assign(ssm_to_sv(cont->second_event), act->priority, EVENT_VALUE);
+      ssm_later(ssm_to_sv(cont->timer), ssm_now() + SSM_SECOND, EVENT_VALUE);
       cont->trigger1.act = act;
-      ssm_sensitize(&cont->timer, &cont->trigger1);
+      ssm_sensitize(ssm_to_sv(cont->timer), &cont->trigger1);
       act->pc = 1;
       return;
     case 1:
       ssm_desensitize(&cont->trigger1);
     }
   }
-  act = ssm_leave(&cont->act);
-  free(cont);
-  if (act)
-    ssm_call(act);
+  ssm_leave(&cont->act, sizeof(*cont));
 }
 
 ssm_stepf_t step_report_seconds;
 
 ssm_act_t *ssm_enter_report_seconds(struct ssm_act *parent,
                                     ssm_priority_t priority, ssm_depth_t depth,
-                                    ssm_event_t *second_event) {
-  act_report_seconds_t *cont = malloc(sizeof(*cont));
-  ssm_enter(&cont->act, step_report_seconds, parent, priority, depth);
+                                    ssm_value_t second_event) {
+  act_report_seconds_t *cont =
+      container_of(ssm_enter(sizeof(act_report_seconds_t), step_report_seconds,
+                             parent, priority, depth),
+                   act_report_seconds_t, act);
   cont->second_event = second_event;
-  ssm_initialize(&cont->seconds);
-  cont->seconds.value = ssm_marshal(0);
   return &cont->act;
 }
 
@@ -94,23 +94,22 @@ void step_report_seconds(struct ssm_act *act) {
 
   switch (act->pc) {
   case 0:
+    cont->seconds = ssm_from_sv(ssm_new_sv(ssm_marshal(0)));
     for (;;) {
       cont->trigger1.act = act;
-      ssm_sensitize(cont->second_event, &cont->trigger1);
+      ssm_sensitize(ssm_to_sv(cont->second_event), &cont->trigger1);
       act->pc = 1;
       return;
     case 1:
       ssm_desensitize(&cont->trigger1);
-      ssm_assign(&cont->seconds, act->priority,
-                 ssm_marshal(ssm_unmarshal(cont->seconds.value) + 1));
+      ssm_assign(
+          ssm_to_sv(cont->seconds), act->priority,
+          ssm_marshal(ssm_unmarshal(ssm_deref(cont->seconds)) + 1));
 
-      printf("%d\n", (int)ssm_unmarshal(cont->seconds.value));
+      printf("%d\n", (int)ssm_unmarshal(ssm_deref(cont->seconds)));
     }
   }
-  act = ssm_leave(&cont->act);
-  free(cont);
-  if (act)
-    ssm_call(act);
+  ssm_leave(&cont->act, sizeof(*cont));
 }
 
 ssm_stepf_t step_main;
@@ -118,10 +117,9 @@ ssm_stepf_t step_main;
 // Create a new activation record for main
 ssm_act_t *ssm_enter_main(struct ssm_act *parent, ssm_priority_t priority,
                           ssm_depth_t depth) {
-  act_main_t *cont = malloc(sizeof(*cont));
-  ssm_enter(&cont->act, step_main, parent, priority, depth);
-
-  ssm_initialize(&cont->second);
+  act_main_t *cont = container_of(
+      ssm_enter(sizeof(act_main_t), step_main, parent, priority, depth),
+      act_main_t, act);
   return &cont->act;
 }
 
@@ -130,27 +128,26 @@ void step_main(struct ssm_act *act) {
 
   switch (act->pc) {
   case 0: {
+    cont->second = ssm_from_sv(ssm_new_sv(EVENT_VALUE));
     ssm_depth_t new_depth = act->depth - 1;
     ssm_priority_t new_priority = act->priority;
     ssm_priority_t pinc = 1 << new_depth;
     ssm_activate(
-        ssm_enter_second_clock(act, new_priority, new_depth, &cont->second));
+        ssm_enter_second_clock(act, new_priority, new_depth, cont->second));
     new_priority += pinc;
     ssm_activate(
-        ssm_enter_report_seconds(act, new_priority, new_depth, &cont->second));
+        ssm_enter_report_seconds(act, new_priority, new_depth, cont->second));
   }
     act->pc = 1;
     return;
   case 1:
     break;
   }
-  act = ssm_leave(&cont->act);
-  free(cont);
-  if (act)
-    ssm_call(act);
+  ssm_leave(&cont->act, sizeof(*cont));
 }
 
-void ssm_throw(int reason, const char *file, int line, const char *func) {
+void ssm_throw(enum ssm_error reason, const char *file, int line,
+               const char *func) {
   printf("SSM error at %s:%s:%d: reason: %d\n", file, func, line, reason);
   exit(1);
 }
--- examples/fib3.c
+++ examples/fib3.c
@@ -1,4 +1,5 @@
-#include <ssm-types.h>
+#include <ssm-internal.h>
+#include <ssm-typedefs.h>
 #include <ssm.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,28 +29,29 @@ fib (n: Int) (&r: &Int) =
 
 typedef struct {
   ssm_act_t act;
-  ssm_u32_t *r;
+  ssm_u32_t r;
   struct ssm_trigger trigger1;
 } act_mywait_t;
 
 typedef struct {
   ssm_act_t act;
-  ssm_u32_t *r1, *r2, *r;
+  ssm_u32_t r1, r2, r;
 } act_sum_t;
 
 typedef struct {
   ssm_act_t act;
   u32 n;
-  ssm_u32_t *r;
+  ssm_u32_t r;
   ssm_u32_t r1, r2;
 } act_fib_t;
 
 ssm_stepf_t step_mywait;
 
 ssm_act_t *ssm_enter_mywait(struct ssm_act *parent, ssm_priority_t priority,
-                            ssm_depth_t depth, ssm_u32_t *r) {
-  act_mywait_t *cont = malloc(sizeof(*cont));
-  ssm_enter(&cont->act, step_mywait, parent, priority, depth);
+                            ssm_depth_t depth, ssm_u32_t r) {
+  act_mywait_t *cont = container_of(
+      ssm_enter(sizeof(*cont), step_mywait, parent, priority, depth),
+      act_mywait_t, act);
   cont->r = r;
   return &cont->act;
 }
@@ -59,26 +61,24 @@ void step_mywait(struct ssm_act *act) {
   switch (act->pc) {
   case 0:
     cont->trigger1.act = act;
-    ssm_sensitize(cont->r, &cont->trigger1);
+    ssm_sensitize(ssm_to_sv(cont->r), &cont->trigger1);
     act->pc = 1;
     return;
   case 1:
     ssm_desensitize(&cont->trigger1);
     break;
   }
-  act = ssm_leave(act);
-  free(cont);
-  if (act)
-    ssm_call(act);
+  ssm_leave(&cont->act, sizeof(*cont));
 }
 
 ssm_stepf_t step_sum;
 
 ssm_act_t *ssm_enter_sum(struct ssm_act *parent, ssm_priority_t priority,
-                         ssm_depth_t depth, ssm_u32_t *r1, ssm_u32_t *r2,
-                         ssm_u32_t *r) {
-  act_sum_t *cont = malloc(sizeof(*cont));
-  ssm_enter(&cont->act, step_sum, parent, priority, depth);
+                         ssm_depth_t depth, ssm_u32_t r1, ssm_u32_t r2,
+                         ssm_u32_t r) {
+  act_sum_t *cont =
+      container_of(ssm_enter(sizeof(*cont), step_sum, parent, priority, depth),
+                   act_sum_t, act);
   cont->r1 = r1;
   cont->r2 = r2;
   cont->r = r;
@@ -99,27 +99,25 @@ void step_sum(struct ssm_act *act) {
     act->pc = 1;
     return;
   case 1:
-    ssm_later(cont->r, ssm_now() + SSM_SECOND,
-              ssm_marshal(ssm_unmarshal(cont->r1->value) +
-                          ssm_unmarshal(cont->r2->value)));
+    ssm_later(ssm_to_sv(cont->r), ssm_now() + SSM_SECOND,
+              ssm_marshal(ssm_unmarshal(ssm_deref(cont->r1)) +
+                          ssm_unmarshal(ssm_deref(cont->r2))));
     break;
   }
-  act = ssm_leave(act);
-  free(cont);
-  if (act)
-    ssm_call(act);
+  ssm_leave(&cont->act, sizeof(*cont));
 }
 
 ssm_stepf_t step_fib;
 
 ssm_act_t *ssm_enter_fib(struct ssm_act *parent, ssm_priority_t priority,
-                         ssm_depth_t depth, u32 n, ssm_u32_t *r) {
-  act_fib_t *cont = malloc(sizeof(*cont));
-  ssm_enter(&cont->act, step_fib, parent, priority, depth);
+                         ssm_depth_t depth, u32 n, ssm_u32_t r) {
+  act_fib_t *cont =
+      container_of(ssm_enter(sizeof(*cont), step_fib, parent, priority, depth),
+                   act_fib_t, act);
   cont->n = n;
   cont->r = r;
-  ssm_initialize(&cont->r1);
-  ssm_initialize(&cont->r2);
+  /* ssm_initialize(&cont->r1); */
+  /* ssm_initialize(&cont->r2); */
   return &cont->act;
 }
 
@@ -127,11 +125,11 @@ void step_fib(struct ssm_act *act) {
   act_fib_t *cont = container_of(act, act_fib_t, act);
   switch (act->pc) {
   case 0:
-    cont->r1.value = ssm_marshal(0);
-    cont->r2.value = ssm_marshal(0);
+    cont->r1 = ssm_from_sv(ssm_new_sv(ssm_marshal(0)));
+    cont->r2 = ssm_from_sv(ssm_new_sv(ssm_marshal(0)));
 
     if (ssm_unmarshal(cont->n) < 2) {
-      ssm_later(cont->r, ssm_now() + SSM_SECOND, ssm_marshal(1));
+      ssm_later(ssm_to_sv(cont->r), ssm_now() + SSM_SECOND, ssm_marshal(1));
       break;
     }
     {
@@ -140,40 +138,36 @@ void step_fib(struct ssm_act *act) {
       ssm_priority_t pinc = 1 << new_depth;
       ssm_activate(ssm_enter_fib(act, new_priority, new_depth,
                                  ssm_marshal(ssm_unmarshal(cont->n) - 1),
-                                 &cont->r1));
+                                 cont->r1));
       new_priority += pinc;
       ssm_activate(ssm_enter_fib(act, new_priority, new_depth,
                                  ssm_marshal(ssm_unmarshal(cont->n) - 2),
-                                 &cont->r2));
+                                 cont->r2));
       new_priority += pinc;
-      ssm_activate(ssm_enter_sum(act, new_priority, new_depth, &cont->r1,
-                                 &cont->r2, cont->r));
+      ssm_activate(ssm_enter_sum(act, new_priority, new_depth, cont->r1,
+                                 cont->r2, cont->r));
     }
     act->pc = 1;
     return;
   case 1:
     break;
   }
-  act = ssm_leave(act);
-  free(cont);
-  if (act)
-    ssm_call(act);
+  ssm_leave(&cont->act, sizeof(*cont));
 }
 
-void ssm_throw(int reason, const char *file, int line, const char *func) {
+void ssm_throw(enum ssm_error reason, const char *file, int line,
+               const char *func) {
   printf("SSM error at %s:%s:%d: reason: %d\n", file, func, line, reason);
   exit(1);
 }
 
 int main(int argc, char *argv[]) {
-  ssm_u32_t result;
-  ssm_initialize(&result);
-  result.value = ssm_marshal(12);
+  ssm_u32_t result = ssm_from_sv(ssm_new_sv(ssm_marshal(0xdeadbeef)));
 
   int n = argc > 1 ? atoi(argv[1]) : 3;
 
   ssm_activate(ssm_enter_fib(&ssm_top_parent, SSM_ROOT_PRIORITY, SSM_ROOT_DEPTH,
-                             ssm_marshal(n), &result));
+                             ssm_marshal(n), result));
 
   ssm_tick();
 
@@ -181,7 +175,7 @@ int main(int argc, char *argv[]) {
     ssm_tick();
 
   printf("simulated %lu seconds\n", ssm_now() / SSM_SECOND);
-  printf("%d\n", (int)ssm_unmarshal(result.value));
+  printf("%d\n", (int)ssm_unmarshal(ssm_deref(result)));
 
   return 0;
 }
