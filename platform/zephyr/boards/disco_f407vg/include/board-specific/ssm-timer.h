a0dad5eeecbb7a5fbd50b6093be0cec279fc519b
j-hui <j-hui@cs.columbia.edu>
2022-04-26 04:14:55 -0400

    Remove unrelated platformio stuff to clean up PR

--- platform/zephyr/boards/disco_f407vg/include/board-specific/ssm-timer.h
+++
@@ -1,6 +0,0 @@
-#ifndef _BOARD_SPECIFIC_SSM_TIMER_H
-#define _BOARD_SPECIFIC_SSM_TIMER_H
-
-// BOARD_TIMER_INIT() would be defined here if needed
-
-#endif /* _BOARD_SPECIFIC_SSM_TIMER_H */
--- platform/zephyr/include/platform-specific/ssm-prof.h
+++
@@ -1,6 +0,0 @@
-#ifndef _PLATFORM_SPECIFIC_SSM_PROF_H
-#define _PLATFORM_SPECIFIC_SSM_PROF_H
-
-#include <board-specific/ssm-prof.h>
-
-#endif /* _PLATFORM_SPECIFIC_SSM_PROF_H */
