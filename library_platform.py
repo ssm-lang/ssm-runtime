deab690191fe66c4dc0b60a04daff1b6b994c320
j-hui <j-hui@cs.columbia.edu>
2022-07-27 14:20:42 -0400

    wIP

--- library_platform.py
+++ library_platform.py
@@ -4,6 +4,10 @@ from os.path import join, realpath
 platform = env['PIOFRAMEWORK'][0]
 board = env['PIOENV']
 
+print("hello world")
+print("platform: ", platform)
+print("board: ", board)
+
 platform_dir = realpath(join("platform", platform))
 boards_dir = realpath(join(platform_dir, "boards"))
 board_dir = realpath(join(boards_dir, board))
@@ -11,8 +15,8 @@ board_dir = realpath(join(boards_dir, board))
 # Specify platform/board includes
 env.Append(
     CPPPATH=[
-        realpath(join(platform_dir, "include")),
-        realpath(join(board_dir, "include")),
+        realpath(join("platform", platform, "include")),
+        realpath(join("platform", platform, "boards", board, "include")),
     ]
 )
 # Specify common and platform source directories
@@ -20,16 +24,15 @@ env.Replace(
     SRC_FILTER=[
         "-<*>",
         "+<src>",
-        "+<%s>" % platform_dir,
-        "-<%s>" % boards_dir,
-        "+<%s>" % board_dir,
+        "+<%s>" % realpath(join("platform", platform, "src")),
+        "+<%s>" % realpath(join("platform", platform, "boards", board, "src")),
     ]
 )
 # Platform defines
 env.Append(
     CPPDEFINES=[
-        # This will be needed if we ever have a 64-bit timer platform,
-        # to set PLATFORM_TIMER64; e.g.:
-        # "PLATFORM_TIMER64",
+      # This will be needed if we ever have a 64-bit timer platform,
+      # to set PLATFORM_TIMER64; e.g.:
+      # "PLATFORM_TIMER64",
     ]
 )
