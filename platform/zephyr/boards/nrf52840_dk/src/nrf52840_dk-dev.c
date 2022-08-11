b41c44955bafee0a1d912152af44c1bf3d2e1e72
j-hui <j-hui@cs.columbia.edu>
2022-08-11 15:49:39 -0400

    Disable zephyr dev stuff

--- platform/zephyr/boards/nrf52840_dk/src/nrf52840_dk-dev.c
+++ platform/zephyr/boards/nrf52840_dk/src/nrf52840_dk-dev.c
@@ -1,5 +1,7 @@
 #include <platform/ssm-dev.h>
 
+#if 0 // This subsystem isn't ready yet
+
 SSM_ZEPHYR_DEFINE_INPUT_DEVICE_GPIO(sw0, ssm_marshal(0),
                                     GPIO_INT_EDGE_TO_ACTIVE);
 SSM_ZEPHYR_DEFINE_INPUT_DEVICE_GPIO(sw1, ssm_marshal(0),
@@ -26,3 +28,4 @@ int ssm_zephyr_initialize_board_devices(ssm_act_t *parent) {
   SSM_ZEPHYR_INIT_OUTPUT_DEVICE_GPIO(led3);
   return 0;
 }
+#endif // #if 0
