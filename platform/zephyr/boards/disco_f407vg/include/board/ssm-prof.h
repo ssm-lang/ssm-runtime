132242631a2bbdaa2a7f584fc3ee2d2a5ad7fbb6
j-hui <j-hui@cs.columbia.edu>
2022-08-12 15:40:58 -0400

    Remove some unused, unuseful stuff

--- platform/zephyr/boards/disco_f407vg/include/board/ssm-prof.h
+++
@@ -1,6 +0,0 @@
-#ifndef _BOARD_SSM_PROF_H
-#define _BOARD_SSM_PROF_H
-
-// Don't need anything here for now
-
-#endif
--- platform/zephyr/boards/disco_f407vg/include/board/ssm-timer.h
+++
@@ -1,6 +0,0 @@
-#ifndef _BOARD_SSM_TIMER_H
-#define _BOARD_SSM_TIMER_H
-
-// BOARD_TIMER_INIT() would be defined here if needed
-
-#endif /* _BOARD_SSM_TIMER_H */
